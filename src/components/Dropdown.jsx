61397b33ba8760005ff8a5c1293174b1e5a992f1
solenedel <solene98@student.ubc.ca>
2022-01-28 09:32:04 -0800

    work on dropown section

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -5,11 +5,12 @@ const Dropdown = ({ getData }) => {
   return (
     <div id="dropdown-section">
       <button type="button" onClick={() => getData("ppp1")}>
-        ppp1 data
+        $1.90/day (2011 ppp)
       </button>
       <button type="button" onClick={() => getData("ppp2")}>
-        ppp2 data
+        $3.10/day (2011 ppp)
       </button>
+      <div id="dropdown-text">About this graph.</div>
       {/* {data.length && console.log("DATA STATE: ", data[0].gender)} */}
     </div>
   );
