c2103419cd1c5673e6c2973102afb55a0df2031e
solenedel <solene98@student.ubc.ca>
2022-01-28 09:22:57 -0800

    fix bug- displaying genders correctly

--- src/components/PovertyChart.jsx
+++ src/components/PovertyChart.jsx
@@ -4,13 +4,14 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 const PovertyChart = ({ graphData }) => {
   return (
     <BarChart width={600} height={300} data={graphData}>
-      <XAxis dataKey={graphData.gender} stroke="#8884d8" />
+      <XAxis dataKey="gender" stroke="white" />
       <YAxis />
       <Tooltip />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <Bar dataKey="sevpov" fill="#8884d8" barSize={30} />
       <Bar dataKey="povgap" fill="#84d8b5" barSize={30} />
       <Bar dataKey="hc" fill="#c7d884" barSize={30} />
+      {graphData.length && console.log("FORMATTED DATA: ", graphData)}
     </BarChart>
   );
 };
