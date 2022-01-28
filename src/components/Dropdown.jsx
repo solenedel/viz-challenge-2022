5f2cda9b2823957faab03f02c4f7ca635354d61d
solenedel <solene98@student.ubc.ca>
2022-01-28 10:05:33 -0800

    style radio buttons

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -6,23 +6,28 @@ const Dropdown = ({ setPpp }) => {
     <div id="dropdown-section">
       <h2>Wage</h2>
       <div className="dropdown">
-        <label htmlFor="ppp1">$1.90/day (2011 ppp)</label>
-        <input
-          type="radio"
-          id="ppp1"
-          name="ppp"
-          value="ppp1"
-          defaultChecked
-          onClick={() => setPpp("ppp1")}
-        />
-        <label htmlFor="ppp1">$3.10/day (2011 ppp)</label>
-        <input
-          type="radio"
-          id="ppp2"
-          name="ppp"
-          value="ppp2"
-          onClick={() => setPpp("ppp2")}
-        />
+        <span>
+          <input
+            type="radio"
+            id="ppp1"
+            name="ppp"
+            value="ppp1"
+            defaultChecked
+            onClick={() => setPpp("ppp1")}
+          />
+          <label htmlFor="ppp1">$1.90/day (2011 ppp)</label>
+        </span>
+        <span>
+          {" "}
+          <input
+            type="radio"
+            id="ppp2"
+            name="ppp"
+            value="ppp2"
+            onClick={() => setPpp("ppp2")}
+          />
+          <label htmlFor="ppp1">$3.10/day (2011 ppp)</label>
+        </span>
       </div>
 
       <div id="dropdown-text">
