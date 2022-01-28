e40e9dc66c77a3cae6c720deedd4dfaf11efa348
solenedel <solene98@student.ubc.ca>
2022-01-28 08:35:40 -0800

    lift data state to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import BarChart from "./components/BarChart";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  // request data for ppp1 OR ppp2 from backend
+  const getData = (ppp) => {
+    axios
+      .get(`http://localhost:8081/data/${ppp}`)
+      .then((res) => {
+        setData([...res.data]);
+        // console.log(`DATA for ${ppp} `, res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="App">
       <h1>Poverty Measures by Gender</h1>
       <main>
-        <Dropdown />
+        <Dropdown getData={getData} />
         <BarChart />
       </main>
     </div>
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,22 +1,6 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
-const Dropdown = () => {
-  const [data, setData] = useState([]);
-
-  // request data for ppp1 OR ppp2 from backend
-  const getData = (ppp) => {
-    axios
-      .get(`http://localhost:8081/data/${ppp}`)
-      .then((res) => {
-        setData([...res.data]);
-        // console.log(`DATA for ${ppp} `, res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
+const Dropdown = ({ getData }) => {
   // TODO - refactor getData to make more modular if time
   return (
     <div id="dropdown-section">
@@ -26,7 +10,7 @@ const Dropdown = () => {
       <button type="button" onClick={() => getData("ppp2")}>
         ppp2 data
       </button>
-      {data.length && console.log("DATA STATE: ", data[0].gender)}
+      {/* {data.length && console.log("DATA STATE: ", data[0].gender)} */}
     </div>
   );
 };
