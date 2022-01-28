d023c8f0362a7c64f2041184905962cd69241a9c
solenedel <solene98@student.ubc.ca>
2022-01-28 08:32:06 -0800

    check state of data

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,18 +4,20 @@ import axios from "axios";
 const Dropdown = () => {
   const [data, setData] = useState([]);
 
+  // request data for ppp1 OR ppp2 from backend
   const getData = (ppp) => {
     axios
       .get(`http://localhost:8081/data/${ppp}`)
       .then((res) => {
         setData([...res.data]);
-        console.log("DATA TO FRONTEND: ", res);
+        // console.log(`DATA for ${ppp} `, res);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  // TODO - refactor getData to make more modular if time
   return (
     <div id="dropdown-section">
       <button type="button" onClick={() => getData("ppp1")}>
@@ -24,6 +26,7 @@ const Dropdown = () => {
       <button type="button" onClick={() => getData("ppp2")}>
         ppp2 data
       </button>
+      {data.length && console.log("DATA STATE: ", data[0].gender)}
     </div>
   );
 };
